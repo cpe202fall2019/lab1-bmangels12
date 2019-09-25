6760151c1232c597fd82783140a7671a574eae17
Ben Mangelsdorf <benmangelsdorf@Bens-MacBook-Pro.local>
2019-09-25 10:44:18 -0700

    9/25 changes

--- lab1.py
+++ lab1.py
@@ -2,14 +2,37 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+   if int_list == None:
+      raise ValueError
+      return
+   if len(int_list) < 1:
+       return None
+   largest = int_list[0]
+   for num in int_list: 
+       if num > largest:
+           largest = num
+   return largest
 
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   pass
+   if int_list == None:
+      raise ValueError
+      return
+   if len(int_list) == 0:
+      return int_list
+   return int_list[-1:] + reverse_rec(int_list[:-1])   
+
 
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
    pass
+   if int_list == None:
+      raise ValueError
+      return 
+   if low == high:
+      return None
+   if int_list[low] == target:
+      return low
+   return bin_search(target, low + 1, high, int_list)
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -1,4 +1,4 @@
-aimport unittest
+import unittest
 from lab1 import *
 
  # A few test cases.  Add more!!!
@@ -9,15 +9,42 @@ class TestLab1(unittest.TestCase):
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
+    def test_max_list_iter2(self):
+        # checking if the function works correctly, should result in 4
+        list = [1, 2, 3, 4]
+        self.assertEqual(max_list_iter(list), 4)
+    def test_max_list_iter3(self):
+        # checking if an empty list results in None
+        list = []
+        self.assertEqual(max_list_iter(list), None)
+
 
     def test_reverse_rec(self):
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+    def test_reverse_rec2(self):
+        #second test of list   
+        self.assertEqual(reverse_rec([3,2,1]),[1,2,3])    
+    def test_reverse_rec3(self):
+        #to test for value error
+        tlist = None
+        with self.assertRaises(ValueError):
+            reverse_rec(tlist)
 
     def test_bin_search(self):
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
         self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+    def test_bin_search2(self):
+        tlist = None
+        with self.assertRaises(ValueError):  # used to check for exception
+            bin_search(1, 2, 3, tlist)
+    def test_bin_search3(self):
+        list_val = [1,2,3,4]
+        low = 1
+        high = 3
+        self.assertEqual(bin_search(1, low, high, list_val), None)
+
 
 if __name__ == "__main__":
         unittest.main()
